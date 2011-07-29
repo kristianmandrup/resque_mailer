aa6a3aa0378f8b0f1e45ee54411fa86659107ef1
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2011-07-29 00:48:59 -0400

    Add a default value to the time argument

--- lib/resque_mailer.rb
+++ lib/resque_mailer.rb
@@ -30,8 +30,8 @@ module Resque
           mailer_class = self
           super.tap do |resque_mail|
             resque_mail.class_eval do
-              define_method(:deliver) do |time|
-                resque.enqueue_in(time=5, mailer_class, method_name, *args)
+              define_method(:deliver) do |time=5|
+                resque.enqueue_in(time, mailer_class, method_name, *args)
                 self
               end
             end
